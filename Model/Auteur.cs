925f0ae2dea6c694e3a9d5b5afcef1c3f082388d
thomas-luyckx <95084337+thomas-luyckx@users.noreply.github.com>
2023-03-28 08:57:35 +0200

    Add project files.

---
+++ Model/Auteur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEFTutorial.Model {
+    public class Auteur {
+        public Auteur(string naam, string emailContact) {
+            Naam = naam;
+            EmailContact = emailContact;
+        }
+
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string EmailContact { get; set; }
+    }
+}
